e59961b746776fee87aa1d7dad8d3598d7401e83
atallison <tanner@atallison.com>
2023-05-11 12:20:20 -0500

    Adding Terraform

---
+++ deploy.tf
@@ -0,0 +1,30 @@
+﻿terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.64.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  credentials = file("mygcp-credentials.json")
+  project = "mineral-music-331421"
+  region  = "us-central1"
+  zone = "us-central1-a"
+}
+
+resource "google_compute_instance" "my_server" {
+  machine_type = "e2-micro"
+  name         = "my-gcp-server"
+  boot_disk {
+    initialize_params {
+      image = "debian-11-bullseye-v20230509"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
